24fc2c0262cd7117025a6211d0e46dc840e82e85
xisigui <nagato.leet@gmail.com>
2025-07-18 22:22:04 +0800

    feat(frontend): Added data fetch and displaying it to table

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,25 @@
+"use client";
 import { DataTable } from "@/components/data-table";
 import SampleData from "./data.json";
 import { ProfileForm } from "@/components/form-profile";
 import { Button } from "@/components/ui/button";
 import { UserRoundPlus } from "lucide-react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const [profiles, setProfiles] = useState([]);
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+  const fetchData = async () => {
+    const result = await axios.get(`${apiUrl}/api/UserProfiles/`);
+    setProfiles(result.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="font-sans items-center justify-items-center min-h-screen p-5">
       <div className="flex flex-col gap-2 items-start">
@@ -16,7 +31,7 @@ export default function Home() {
             </Button>
           }
         />
-        <DataTable data={SampleData} />
+        <DataTable data={profiles} />
       </div>
     </div>
   );
