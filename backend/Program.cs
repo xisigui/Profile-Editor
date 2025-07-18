44820868bb720cdabb1ff71a5d5ae40635e8ad0b
xisigui <nagato.leet@gmail.com>
2025-07-18 22:17:10 +0800

    feat: Implemented cors whitelist

--- backend/Program.cs
+++ backend/Program.cs
@@ -8,17 +8,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-
+app.UseCors("AllowFrontend");
 app.UseAuthorization();
 app.MapControllers();
 app.UseHttpsRedirection();
