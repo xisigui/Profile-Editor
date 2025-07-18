b65f46c74f5f2a8fcce1257739ae8c024499b9d7
xisigui <nagato.leet@gmail.com>
2025-07-18 14:25:55 +0800

    feat(table-ui): Added table pagination

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,7 +5,10 @@ export default function Home() {
   return (
     <div className="font-sans items-center justify-items-center min-h-screen p-8">
       <div className="flex flex-col gap-[32px] row-start-2 items-start">
+        {/* Table With Data */}
         <DataTable data={SampleData} />
+
+        {/* Empty Table */}
         <DataTable data={[]} />
       </div>
     </div>
--- frontend/components/data-table.tsx
+++ frontend/components/data-table.tsx
@@ -10,6 +10,10 @@ import {
 } from "@/components/ui/table";
 import { Label } from "@/components/ui/label";
 import {
+  IconChevronLeft,
+  IconChevronRight,
+  IconChevronsLeft,
+  IconChevronsRight,
   IconDotsVertical,
   IconGenderFemale,
   IconGenderMale,
@@ -28,6 +32,8 @@ import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 import { format } from "date-fns";
@@ -158,6 +164,8 @@ export function DataTable({ data }: { data: z.infer<typeof schema>[] }) {
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
   });
 
   return (
@@ -211,6 +219,58 @@ export function DataTable({ data }: { data: z.infer<typeof schema>[] }) {
             )}
           </TableBody>
         </Table>
+      </div>
+      <div className="flex items-center justify-between px-4">
+        <div className="text-muted-foreground hidden flex-1 text-sm lg:flex">
+          {table.getFilteredRowModel().rows.length} total item(s) found.
+        </div>
+        <div className="flex w-full items-center gap-8 lg:w-fit">
+          <div className="flex w-fit items-center justify-center text-sm font-medium">
+            Page {table.getState().pagination.pageIndex + 1} of{" "}
+            {table.getPageCount()}
+          </div>
+          <div className="ml-auto flex items-center gap-2 lg:ml-0">
+            <Button
+              variant="outline"
+              className="hidden h-8 w-8 p-0 lg:flex"
+              onClick={() => table.setPageIndex(0)}
+              disabled={!table.getCanPreviousPage()}
+            >
+              <span className="sr-only">Go to first page</span>
+              <IconChevronsLeft />
+            </Button>
+            <Button
+              variant="outline"
+              className="size-8"
+              size="icon"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              <span className="sr-only">Go to previous page</span>
+              <IconChevronLeft />
+            </Button>
+            <Button
+              variant="outline"
+              className="size-8"
+              size="icon"
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              <span className="sr-only">Go to next page</span>
+              <IconChevronRight />
+            </Button>
+            <Button
+              variant="outline"
+              className="hidden size-8 lg:flex"
+              size="icon"
+              onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+              disabled={!table.getCanNextPage()}
+            >
+              <span className="sr-only">Go to last page</span>
+              <IconChevronsRight />
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
